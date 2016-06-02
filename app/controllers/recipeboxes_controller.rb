1bf3ec0a6654cfce07638e700d95423c0faf8968
Hewan Abreha <henebys@gmail.com>
2016-06-01 20:20:36 -0400

    defined show, new, create and recipe params, created recipe object with user id

--- app/controllers/recipeboxes_controller.rb
+++ app/controllers/recipeboxes_controller.rb
@@ -1,25 +1,33 @@
 class RecipeboxesController < ApplicationController
 
-def index
-  @recipeboxes = Recipebox.all
-end
+  def index
+    @recipeboxes = Recipebox.all
+  end
 
-def show
-  @recipebox = Recipebox.find(params[:id])
-end
-#
-# def new
-#   @recipebox = Recipebox.new
-# end
-#
-#
-# def create
-#   @recipebox = Recipebox.new(recipebox_params)
-#
-# end
-#
-# private
-#
-# def recipebox_params
-#   params.require(:recipebox).permit()
+  def show
+    @recipebox = Recipebox.find(params[:id])
+  end
+
+  def new
+    @recipebox = Recipebox.new
+  end
+
+  def create
+    @recipebox = Recipebox.new(recipebox_params)
+    @recipebox.user = User.find(2)
+
+    if @recipebox.save
+      flash[:success] = "You have created your recipe successfully!"
+      redirect_to recipeboxes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipebox_params
+    params.require(:recipebox).permit(:name, :summary, :description)
+  end
+
 end
